6911c7274df6bcdbdce870e467e1a7acf7695f2a
vinothvsbe <vinothvsbe@gmail.com>
2020-12-23 13:58:21 +0530

    Switch case pattern matching example

--- SwitchCasePatternMatching/Program.cs
+++ SwitchCasePatternMatching/Program.cs
@@ -6,12 +6,34 @@ namespace SwitchCasePatternMatching
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args[0]);
-            Console.WriteLine("\nWhat is your name? ");
-            var name = Console.ReadLine();
-            var date = DateTime.Now;
-            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
-            Console.Write("\nPress any key to exit...");
+            Console.WriteLine("Language choice");
+            Console.WriteLine("1 => Python");
+            Console.WriteLine("2 => C#");
+            Console.WriteLine("3 => GoLang");
+            Console.WriteLine("4 => Haskel");
+            var userInput = Console.ReadLine();
+            object userChoice = int.TryParse(userInput.ToString(), out int val)?val:userInput;
+            switch(userChoice){
+                case int i when i==1:
+                case string s when s.ToLower()=="python":
+                    Console.WriteLine("Selected Python");
+                break;
+                case int i when i==2:
+                case string s when s.ToLower()=="c#":
+                    Console.WriteLine("Selected C#");
+                break;
+                case int i when i==3:
+                case string s when s.ToLower()=="golang":
+                    Console.WriteLine("Selected GoLang");
+                break;
+                case int i when i==4:
+                case string s when s.ToLower()=="haskel":
+                    Console.WriteLine("Selected Haskel");
+                break;
+                default:
+                    Console.WriteLine("Selected language not in the list");
+                break;
+            }
             Console.ReadKey(true);
         }
     }
