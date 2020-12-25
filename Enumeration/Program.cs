1fe2042c0ab3824ab3d843917e3fd7ee9ec7412d
vinothvsbe <vinothvsbe@gmail.com>
2020-12-25 20:22:15 +0530

    Enumerations

---
+++ Enumeration/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------IEnumerator----------");
+            Garage carLot=new Garage();
+            foreach (Car car in carLot)
+            {
+                Console.WriteLine(car.CarName);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Car
+    {
+        public string CarName { get; set; }
+        public int Speed { get; set; }
+        public Car(string carName, int speed)
+        {
+            CarName = carName;
+            Speed = speed;
+        }         
+    }
+
+    public class Garage: IEnumerable
+    {
+        public Car[] carArray = new Car[5];
+        public Garage()
+        {
+            carArray[0]=new Car("Magnite", 140);
+            carArray[1]=new Car("Kwid", 130);
+            carArray[2]=new Car("Nexon", 130);
+            carArray[3]=new Car("Innova", 180);
+            carArray[4]=new Car("XUV", 170);
+        }
+        public IEnumerator GetEnumerator(){
+            return carArray.GetEnumerator();
+        }
+    }
+}
---
+++ InterfaceDefaultImplementation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterfaceDefaultImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square sq = new Square();
+            sq.NumberOfSides = 5;
+            sq.SideLength=15;
+            Console.WriteLine($"Permieter of Square is {((IRegularPointy)sq).Perimeter}");
+            Console.WriteLine($"Aread of Square is {((IRegularPointy)sq).area()}");
+        }
+    }
+
+    interface IRegularPointy{
+        public int SideLength { get; set; }
+        public int NumberOfSides { get; set; }
+        int Perimeter => SideLength * NumberOfSides;
+        int area(){return (SideLength*SideLength);}
+    }
+
+    class Square : IRegularPointy
+    {
+        public void Draw(){
+            Console.WriteLine($"Drawing Square");
+        }
+        public int SideLength { get; set; }
+        public int NumberOfSides { get; set; }
+    }
+
+}
